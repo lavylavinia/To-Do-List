027de497c14b6ad6cb36bc3a14e2dd9cf7d22775
lavylavinia <116091282+lavylavinia@users.noreply.github.com>
2023-03-16 07:43:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Linq;
+using System.Collections;
+
+namespace Agenda
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+    public class Task
+    {   private string task, deadline;
+        //constructori fara parametri
+        public Task()
+        {
+            task = string.Empty;
+            deadline = string.Empty;
+        }
+        //constructori cu parametrii
+        public Task(string _todo, string _deadline)
+        {
+            task = _todo;
+            deadline = _deadline;
+        }
+        //afiseaza data la care a fost creat
+        public string CreatedDate
+        {
+           // get { return; }
+            set { }
+        }
+        //returneaza daca task-ul a fost rezolvat sau nu
+        public bool TaskDone
+        {
+            get { return false; }
+     
+        }
+        //returneaza statusul task-ului
+        public bool TaskStatus
+        {
+            get { return false; }
+        }
+        //returneaza info despre task
+        public string TaskInfo()
+        {
+            return $"Task:";
+        }
+
+    }
+    public class ToDoList
+    {
+        //creare lista de task-uri
+        List<string> AllTasks = new List<string>();
+        public ToDoList()
+        {
+
+        }
+        public void AddTask()
+        {
+
+        }
+        public void DeleteTask()
+        {
+
+        }
+
+    }
+}
+
